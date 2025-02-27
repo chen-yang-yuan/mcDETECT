9f2e199f01581e83cd7bea5b92aa2ef7da603959
Chenyang Yuan <chenyang.yuan@emory.edu>
2025-02-27 16:22:03 -0500

    Bump version to 1.0.8

--- mcDETECT_package/setup.py
+++ mcDETECT_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "mcDETECT",
-    version = "1.0.7",
+    version = "1.0.8",
     packages = find_packages(),
     install_requires = ["anndata", "miniball", "numpy", "pandas", "rtree", "scanpy", "scikit-learn", "scipy", "shapely"],
     author = "Chenyang Yuan",
