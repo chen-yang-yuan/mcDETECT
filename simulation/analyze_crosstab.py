6a4c771429f6e7b44d46fa2f219d1f03953a3ac3
Chenyang Yuan <chenyang.yuan@emory.edu>
2026-03-20 14:46:25 -0400

    Auto-commit: 2026-03-20 14:46:24

--- simulation/analyze_crosstab.py
+++ simulation/analyze_crosstab.py
@@ -267,9 +267,9 @@ def summarize_across_seeds(
 def main():
     base_out = "output"
     methods = {
-        # "mcDETECT": os.path.join(base_out, "mcDETECT_output", "xtabs"),
+        "mcDETECT": os.path.join(base_out, "mcDETECT_output", "xtabs"),
         "Baysor": os.path.join(base_out, "Baysor_output", "xtabs"),
-        # "SSAM": os.path.join(base_out, "SSAM_output", "xtabs"),
+        "SSAM": os.path.join(base_out, "SSAM_output", "xtabs"),
     }
 
     for method_name, xtab_dir in methods.items():
